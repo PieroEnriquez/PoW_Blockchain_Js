97828e53b1cbe72bbef61b959419a06483fdea8c
Piero Enriquez <pieroenriquezvera@gmail.com>
2022-08-23 15:46:24 -0500

    Adding PoW consensus

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = '') {
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
+        this.nonce = 0
     }
 
     calculateHash() {
-        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+        return SHA256(this.timestamp + this.previousHash + JSON.stringify(this.transactions) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash)
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        //The difficulty sets how secure the blockchain can be
+        this.difficulty = 4;
     }
 
     createGenesisBlock() {
-        return new Block(0, "20/08/2022", "Genesis block", "0");
+        return new Block("20/08/2022", "Genesis block", "0");
     }
 
     getLatestBlock() {
@@ -29,7 +48,7 @@ class Blockchain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -52,13 +71,12 @@ class Blockchain {
 }
 
 let myCoin = new Blockchain();
+
+console.log('Mining block 1...');
 myCoin.addBlock(new Block(1, "21/08/2022", { amount: 4 }));
+
+console.log('Mining block 2...');
 myCoin.addBlock(new Block(2, "22/08/2022", { amount: 10 }));
 
-console.log('Is blockchain valid? ' + myCoin.isChainValid());
 
-myCoin.chain[1].data = { amount: 100 };
-myCoin.chain[1].hash = myCoin.chain[1].calculateHash();
-
-console.log('Is blockchain valid? ' + myCoin.isChainValid());
 
